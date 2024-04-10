1bac7241718dc8f9f0ed8f8be1d181577b5cca3f
Kacper Kisielewski <s30670@pjwstk.edu.pl>
2024-04-10 11:05:52 +0200

    rename deadline to date

--- src/Goal.java
+++ src/Goal.java
@@ -1,12 +1,12 @@
 import java.time.LocalDate;
 
 public class Goal {
-    LocalDate deadline;
+    LocalDate date;
     String name;
     int premium;
 
     public Goal(int year, int month, int dayOfMonth, String name, int premium) {
-        this.deadline = LocalDate.of(year, month, dayOfMonth);
+        this.date = LocalDate.of(year, month, dayOfMonth);
         this.name = name;
         this.premium = premium;
     }
